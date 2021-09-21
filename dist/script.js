576824309b0c22508f42c6fa0a7ddb9922b5e9eb
ennnm <jiaen.1sc4@gmail.com>
2021-09-21 10:58:33 +0800

    webpack test

---
+++ dist/script.js
@@ -0,0 +1,25 @@
+/** *** */ (() => { // webpackBootstrap
+  const __webpack_exports__ = {};
+  const component = () => {
+  // make a container
+    const div = document.createElement('div');
+
+    // display element
+    const span = document.createElement('span');
+
+    // input
+    const input = document.createElement('input');
+
+    // on change, display in the span
+    input.addEventListener('change', () => {
+      span.innerText = input.value;
+    });
+    console.log('hey');
+    div.appendChild(input);
+    div.appendChild(span);
+
+    return div;
+  };
+
+  document.body.appendChild(component());
+/** *** */ })();
